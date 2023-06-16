2a752ac3d31e18b8eb2c7132006ec765bdfe431e
Jander Cerqueira <jander_cerqueira@hotmail.com>
2023-06-16 14:26:55 -0300

    Cenário do clique longo

--- features/specs/buttons.feature
+++ features/specs/buttons.feature
@@ -2,8 +2,14 @@
 
 Funcionalidade: Buttons
 
+    @short
     Cenário: Clique Simples
-
         Dado que acesso a tela Click Simples
         Quando faço um click simples
         Então devo ver o texto: "Isso é um clique simples"
+
+    @long
+    Cenário: Clique Longooo
+        Dado que acesso a tela Click Longo
+        Quando faço um click longoo
+        Então devo ver o botão com o texto: "CLIQUE LONGO OK"
--- features/steps/buttons_steps.rb
+++ features/steps/buttons_steps.rb
@@ -14,3 +14,20 @@ Então('devo ver o texto: {string}') do |expect_text|
     #faz uma comparação do texto do toast com o passado na feature
     expect(@toaster.toast.text).to eql expect_text
 end                                                                           
+
+Dado('que acesso a tela Click Longo') do
+     #chama o método que já está implementado no components.rb
+     @nav.tap_hamburguer
+     #chama o método e passa a opção como parâmetro
+     @nav.tap_by_text("BOTÕES")
+     @nav.tap_by_text("CLIQUE LONGO")
+end
+    
+Quando('faço um click longoo') do
+    @button = find_element(id: "io.qaninja.android.twp:id/long_click")
+    Appium::TouchAction.new.press(element: @button).wait(2000).release.perform
+end
+    
+Então('devo ver o botão com o texto: {string}') do |expect_text|
+    expect(@button.text).to eql expect_text
+end
